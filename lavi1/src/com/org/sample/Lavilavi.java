b02276d94a20c454b25421d8d491c7bf26f2314b
dreamerreddy <all4flirt@gmail.com>
2022-09-20 17:10:30 -0500

    Commit

---
+++ lavi1/src/com/org/sample/Lavilavi.java
@@ -0,0 +1,10 @@
+package com.org.sample;
+
+public class Lavilavi 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World !! My name is Chitti Robot");
+	}
+}
---
+++ lavi1/src/com/org/sample/Nameone.java
@@ -0,0 +1,26 @@
+package com.org.sample;
+
+public class Nameone {
+	
+	public static void main(String[] args) {
+		int a=10;
+		double d=20.5;
+		char c='h';
+		boolean b=false;
+		
+		String s="welcom";
+		String n="lavanya";
+		
+		
+	System.out.println('a');
+	System.out.println('b');
+	System.out.println('d');
+	System.out.println('b');
+	System.out.println("s");
+	System.out.println("n");
+	
+		
+		
+	}
+
+}
